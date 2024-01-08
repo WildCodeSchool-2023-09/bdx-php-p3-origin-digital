d2247b58be3b6e251cf10172ecfac219da2c8c62
yoyodes1000 <yoyodes1000@gmail.com>
2024-01-08 11:07:40 +0100

    new migration

--- migrations/Version20240105103328.php
+++ migrations/Version20240105103328.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240105100233 extends AbstractMigration
+final class Version20240105103328 extends AbstractMigration
 {
     public function getDescription(): string
     {
