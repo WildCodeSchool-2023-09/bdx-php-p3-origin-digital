6d777f9247bdd56456ee22c4c6b74a3a92573aeb
yoyodes1000 <yoyodes1000@gmail.com>
2024-01-09 12:33:59 +0100

    crud for section and page created

---
+++ src/Form/SectionType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PageSection;
+use App\Entity\Section;
+use App\Entity\Type;
+use App\Entity\Video;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('slugSection')
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+'choice_label' => 'id',
+            ])
+            ->add('videos', EntityType::class, [
+                'class' => Video::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+            ->add('pageSection', EntityType::class, [
+                'class' => PageSection::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Section::class,
+        ]);
+    }
+}
