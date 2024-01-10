f4cd17b649e56d42877cbf8eeba0b7e9addce967
Julien Ginestal <144034157+Dju0@users.noreply.github.com>
2024-01-10 12:33:45 +0100

    creation edit suppr category + affect video

--- migrations/Version20240110111028.php
+++ migrations/Version20240110111028.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240108101225 extends AbstractMigration
+final class Version20240110111028 extends AbstractMigration
 {
     public function getDescription(): string
     {
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Video;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('video', EntityType::class, [
+                'class' => Video::class,
+                'choice_label' => 'title',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
