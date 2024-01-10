2a3078daffd15483f2cc10ea9d46e72138b04aad
Julien Ginestal <144034157+Dju0@users.noreply.github.com>
2024-01-10 16:16:27 +0100

    fix css form

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -8,22 +8,28 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+        ->add('name', TextType::class, [
+            'label' => false,
+            'attr' => [
+                'placeholder' => 'Nom de la catégorie'
+            ]
+            ])
             ->add('video', EntityType::class, [
+                'label' => 'Choix de la vidéo',
                 'class' => Video::class,
                 'choice_label' => 'title',
                 'multiple' => true,
                 'expanded' => true,
-                'by_reference' => false,
-            ])
-        ;
+            ]);
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
