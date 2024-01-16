1832caa0f922b168de07fa0cf3be2f35ea097b41
Pagot Marion <marion.pagot@gmail.com>
2024-01-16 11:05:04 +0100

    favorites in bdd ok

---
+++ migrations/Version20240115205640.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240115205640 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug_category VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_64C19C157D9DEF6 (slug_category), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category_video (category_id INT NOT NULL, video_id INT NOT NULL, INDEX IDX_94F4956512469DE2 (category_id), INDEX IDX_94F4956529C1004E (video_id), PRIMARY KEY(category_id, video_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE page (id INT AUTO_INCREMENT NOT NULL, page_section_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_140AB620D3C3D2E4 (page_section_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE page_section (id INT AUTO_INCREMENT NOT NULL, ordered INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE section (id INT AUTO_INCREMENT NOT NULL, type_id INT NOT NULL, page_section_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, slug_section VARCHAR(255) NOT NULL, INDEX IDX_2D737AEFC54C8C93 (type_id), INDEX IDX_2D737AEFD3C3D2E4 (page_section_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, photo VARCHAR(255) DEFAULT NULL, role VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video_user (user_id INT NOT NULL, video_id INT NOT NULL, INDEX IDX_8A048B95A76ED395 (user_id), INDEX IDX_8A048B9529C1004E (video_id), PRIMARY KEY(user_id, video_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, file VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, datetime DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', slug_video VARCHAR(255) NOT NULL, is_public TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video_section (video_id INT NOT NULL, section_id INT NOT NULL, INDEX IDX_C89E934229C1004E (video_id), INDEX IDX_C89E9342D823E37A (section_id), PRIMARY KEY(video_id, section_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category_video ADD CONSTRAINT FK_94F4956512469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE category_video ADD CONSTRAINT FK_94F4956529C1004E FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE page ADD CONSTRAINT FK_140AB620D3C3D2E4 FOREIGN KEY (page_section_id) REFERENCES page_section (id)');
+        $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEFC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
+        $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEFD3C3D2E4 FOREIGN KEY (page_section_id) REFERENCES page_section (id)');
+        $this->addSql('ALTER TABLE video_user ADD CONSTRAINT FK_8A048B95A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE video_user ADD CONSTRAINT FK_8A048B9529C1004E FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE video_section ADD CONSTRAINT FK_C89E934229C1004E FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE video_section ADD CONSTRAINT FK_C89E9342D823E37A FOREIGN KEY (section_id) REFERENCES section (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_video DROP FOREIGN KEY FK_94F4956512469DE2');
+        $this->addSql('ALTER TABLE category_video DROP FOREIGN KEY FK_94F4956529C1004E');
+        $this->addSql('ALTER TABLE page DROP FOREIGN KEY FK_140AB620D3C3D2E4');
+        $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEFC54C8C93');
+        $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEFD3C3D2E4');
+        $this->addSql('ALTER TABLE video_user DROP FOREIGN KEY FK_8A048B95A76ED395');
+        $this->addSql('ALTER TABLE video_user DROP FOREIGN KEY FK_8A048B9529C1004E');
+        $this->addSql('ALTER TABLE video_section DROP FOREIGN KEY FK_C89E934229C1004E');
+        $this->addSql('ALTER TABLE video_section DROP FOREIGN KEY FK_C89E9342D823E37A');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE category_video');
+        $this->addSql('DROP TABLE page');
+        $this->addSql('DROP TABLE page_section');
+        $this->addSql('DROP TABLE section');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE video_user');
+        $this->addSql('DROP TABLE video');
+        $this->addSql('DROP TABLE video_section');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
