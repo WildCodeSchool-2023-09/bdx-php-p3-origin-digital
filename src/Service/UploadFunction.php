efb7a13d3b535005b7ec88458391264b604da8ca
Pagot Marion <marion.pagot@gmail.com>
2024-01-16 11:31:38 +0100

    favorites in bdd ok

--- src/Service/UploadFunction.php
+++ src/Service/UploadFunction.php
@@ -1,19 +1,18 @@
 <?php
-/*
+
 namespace App\Service;
 
 use App\Entity\Video;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UploadFunction
 {
-    public function uploadFile(Video $video)
+    public function uploadFile(mixed $file, string $dossier, SluggerInterface $slugger): string
     {
-        $originalVideoName = pathinfo($videoFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $originalVideoName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeVideoName = $slugger->slug($originalVideoName);
-        $newVideoName = $safeVideoName . '-' . uniqid() . '.' . $videoFile->guessExtension();
-        $videoFile->move(dirname(__DIR__, 2) . '/assets/images', $newVideoName);
-        $pimpoye = $video->setFile($newVideoName);
-
-        return $pimpoye;
+        $newVideoName = $safeVideoName . '-' . uniqid() . '.' . $file->guessExtension();
+        $file->move(dirname(__DIR__, 2)  . $dossier, $newVideoName);
+        return $newVideoName;
     }
-}*/
+}
