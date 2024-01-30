b3b7fb5fe05562f54801c8e5eb7ef820fabe1ff6
yoyodes1000 <yoyodes1000@gmail.com>
2024-01-30 21:54:53 +0100

    commit

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -3,10 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\VideoRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use DateTimeImmutable;
 
 #[ORM\Entity(repositoryClass: VideoRepository::class)]
 class Video
@@ -29,7 +29,7 @@ class Video
     private ?string $description = null;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $datetime = null;
+    private ?DateTimeImmutable $datetime = null;
 
     #[ORM\Column(length: 255)]
     private ?string $slugVideo = null;
@@ -51,7 +51,7 @@ class Video
         $this->users = new ArrayCollection();
         $this->categories = new ArrayCollection();
         $this->section = new ArrayCollection();
-        $this->datetime = new \DateTimeImmutable();
+        $this->datetime = new DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -107,12 +107,12 @@ class Video
         return $this;
     }
 
-    public function getDatetime(): ?\DateTimeImmutable
+    public function getDatetime(): ?DateTimeImmutable
     {
         return $this->datetime;
     }
 
-    public function setDatetime(\DateTimeImmutable $datetime): static
+    public function setDatetime(DateTimeImmutable $datetime): static
     {
         $this->datetime = $datetime;
 
